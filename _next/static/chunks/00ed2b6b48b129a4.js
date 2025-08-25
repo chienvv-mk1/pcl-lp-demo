f31434a9e7b763c560416a99a68bff348977ec19
chienvv-mk1 <chienvv@mk1technology.vn>
2025-08-25 18:33:57 +0700

    first commit

---
+++ _next/static/chunks/00ed2b6b48b129a4.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/f704986a5c7a64d4.js",
+  "static/chunks/24c1e764edc2a7eb.js",
+  "static/chunks/turbopack-e889c311246a94e5.js"
+])
---
+++ _next/static/chunks/df32feb9c526b18a.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/4dc4a8481cb08829.js",
+  "static/chunks/24c1e764edc2a7eb.js",
+  "static/chunks/turbopack-ecd09214eb4288de.js"
+])
